2f3c3572e5bcaffdecfdb175f86b19f2f9c879f7
SpaceGhost <lukasz.celitan@gmail.com>
2024-11-10 01:17:30 +0100

    style: fix styles on uart

--- app/modules/uart_text_input/uart_text_input.h
+++ app/modules/uart_text_input/uart_text_input.h
@@ -13,10 +13,8 @@ extern "C" {
 
 /** Text input anonymous structure */
 typedef struct UART_TextInput UART_TextInput;
-typedef void (*UART_TextInputCallback)(void *context);
-typedef bool (*UART_TextInputValidatorCallback)(const char *text,
-                                                FuriString *error,
-                                                void *context);
+typedef void (*UART_TextInputCallback)(void* context);
+typedef bool (*UART_TextInputValidatorCallback)(const char* text, FuriString* error, void* context);
 
 /** Allocate and initialize text input
  *
@@ -24,19 +22,19 @@ typedef bool (*UART_TextInputValidatorCallback)(const char *text,
  *
  * @return     UART_TextInput instance
  */
-UART_TextInput *uart_text_input_alloc();
+UART_TextInput* uart_text_input_alloc();
 
 /** Deinitialize and free text input
  *
  * @param      uart_text_input  UART_TextInput instance
  */
-void uart_text_input_free(UART_TextInput *uart_text_input);
+void uart_text_input_free(UART_TextInput* uart_text_input);
 
 /** Clean text input view Note: this function does not free memory
  *
  * @param      uart_text_input  Text input instance
  */
-void uart_text_input_reset(UART_TextInput *uart_text_input);
+void uart_text_input_reset(UART_TextInput* uart_text_input);
 
 /** Get text input view
  *
@@ -44,7 +42,7 @@ void uart_text_input_reset(UART_TextInput *uart_text_input);
  *
  * @return     View instance that can be used for embedding
  */
-View *uart_text_input_get_view(UART_TextInput *uart_text_input);
+View* uart_text_input_get_view(UART_TextInput* uart_text_input);
 
 /** Set text input result callback
  *
@@ -58,30 +56,30 @@ View *uart_text_input_get_view(UART_TextInput *uart_text_input);
  * @param      clear_default_text  clear text from text_buffer on first OK
  *                                 event
  */
-void uart_text_input_set_result_callback(UART_TextInput *uart_text_input,
-                                         UART_TextInputCallback callback,
-                                         void *callback_context,
-                                         char *text_buffer,
-                                         size_t text_buffer_size,
-                                         bool clear_default_text);
+void uart_text_input_set_result_callback(
+    UART_TextInput* uart_text_input,
+    UART_TextInputCallback callback,
+    void* callback_context,
+    char* text_buffer,
+    size_t text_buffer_size,
+    bool clear_default_text);
 
-void uart_text_input_set_validator(UART_TextInput *uart_text_input,
-                                   UART_TextInputValidatorCallback callback,
-                                   void *callback_context);
+void uart_text_input_set_validator(
+    UART_TextInput* uart_text_input,
+    UART_TextInputValidatorCallback callback,
+    void* callback_context);
 
 UART_TextInputValidatorCallback
-uart_text_input_get_validator_callback(UART_TextInput *uart_text_input);
+    uart_text_input_get_validator_callback(UART_TextInput* uart_text_input);
 
-void *
-uart_text_input_get_validator_callback_context(UART_TextInput *uart_text_input);
+void* uart_text_input_get_validator_callback_context(UART_TextInput* uart_text_input);
 
 /** Set text input header text
  *
  * @param      uart_text_input  UART_TextInput instance
  * @param      text        text to be shown
  */
-void uart_text_input_set_header_text(UART_TextInput *uart_text_input,
-                                     const char *text);
+void uart_text_input_set_header_text(UART_TextInput* uart_text_input, const char* text);
 
 #ifdef __cplusplus
 }
